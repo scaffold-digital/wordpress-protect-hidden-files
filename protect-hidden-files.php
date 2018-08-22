af78c432e1d9c2c1a0cde0d708028d3828496bb5
Rob Sinton <rob@scaffold.digital>
2018-08-22 16:22:23 +0100

    Plugin now refreshes rewrite rules upon activation / deactivation to save the user having to manually refresh permalinks

--- protect-hidden-files.php
+++ protect-hidden-files.php
@@ -5,10 +5,17 @@
 *  Description:    Ensure that hidden (dot) files (such as .git) cannot be accessed on your Wordpress site
 *  Author:         Scaffold Digital
 *  Author URI:     https://scaffold.digital
-*  Version:        1.0.0
+*  Version:        1.0.1
 */
 
 class ScaffoldProtectHiddenFiles {
+
+    protected static $deactivated = false;
+
+    public static function activation_hook()
+    {
+        self::flush_rules();
+    }
    
     public static function add_filter($tag, $function_to_add = null, $priority = 10, $accepted_args = 1)
     {
@@ -16,8 +23,29 @@ class ScaffoldProtectHiddenFiles {
         add_filter($tag, array(__CLASS__, $function_to_add), $priority, $accepted_args);
     }
 
+    public static function deactivation_hook()
+    {
+        self::$deactivated = true;
+        self::flush_rules();
+    }
+
+    public static function flush_rules()
+    {
+        global $wp_rewrite;
+        $wp_rewrite->flush_rules();
+    }
+
+    public static function init()
+    {
+        self::register_deactivation_hook('deactivation_hook');
+        self::add_filter('mod_rewrite_rules');
+        self::register_activation_hook('activation_hook');
+    }
+
     public static function mod_rewrite_rules($rules)
     {
+        if (self::$deactivated) return $rules;
+
         $rule  = "\n# Scaffold - Protect Hidden Files\n";
         $rule .= "<IfModule mod_rewrite.c>\n";
         $rule .= "RewriteEngine On\n";
@@ -27,6 +55,16 @@ class ScaffoldProtectHiddenFiles {
         return $rule . $rules;
     }
 
+    public static function register_activation_hook($function)
+    {
+        register_activation_hook(__FILE__, array(__CLASS__, $function));
+    }
+
+    public static function register_deactivation_hook($function)
+    {
+        register_deactivation_hook(__FILE__, array(__CLASS__, $function));
+    }
+
 }
 
-ScaffoldProtectHiddenFiles::add_filter('mod_rewrite_rules');
+ScaffoldProtectHiddenFiles::init();
